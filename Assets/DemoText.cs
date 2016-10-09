895f127c27d39c9b057de6ff132c2712eadbde31
mmc7 <m.mervecelebi@hotmail.com>
2016-10-09 17:05:00 -0400

    Update DemoText.cs

--- Assets/DemoText.cs
+++ Assets/DemoText.cs
@@ -8,30 +8,14 @@ public class DemoText : MonoBehaviour {
 
 	void Start() {
 		char[] delims = {' '};
-		//textmesh.text = "Hello World";
+	
 		TextAsset txtAssets = (TextAsset)Resources.Load(txtFile);
 		txtContents = txtAssets.text;
 
 		string[] words = txtContents.Split(delims);
 
 		textmesh.text = "Hogwarts";
-		textmesh.color = Color.blue; //new Color(0, 0, 0);
-		//textmesh.text = (s  + "/n") foreach s in words;
-
-		/*for (int i = 0; i < words.Length; i++) {
-			textmesh.text = words [2] + "\n";
-		}
-		*/
-
-
-	    /*for (int i = 0; i < txtContents.Length; i++) {
-			txtSplit = txtContents.Split[0]
-		}
-		textmesh.text = txtContents;
-	   */
+		textmesh.color = Color.blue; 
 	}
 }
-	/*void Update {
 
-	}
-	*/
